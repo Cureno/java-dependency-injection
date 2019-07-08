bdb9ffb3cd814c7acc77efa119cdbc75f463172a
Cureno <14926132+Cureno@users.noreply.github.com>
2019-07-08 16:48:31 +0200

    Annotate dependent

--- dependent/src/main/java/com/blexven/dependent/IndirectHello.java
+++ dependent/src/main/java/com/blexven/dependent/IndirectHello.java
@@ -1,10 +1,15 @@
 package com.blexven.dependent;
 
+import com.blexven.injector.PleaseWork;
+
+@PleaseWork // this is being picked up by "mvn clean package"
 public class IndirectHello {
 
+    @PleaseWork // this is being picked up by "mvn clean package"
     public static void main(String[] args) {
 
-        Greeting greeting = new Hello();
+        @PleaseWork // THIS IS NOT BEING PICKED UP
+                Greeting greeting = new Hello();
 
         greeting.show();
 
