cfe824f23c2818b784be74a52383adfc16a5173b
Cureno <14926132+Cureno@users.noreply.github.com>
2019-07-09 23:40:31 +0200

    Convert annotation to apply to Fields only

--- dependent/src/main/java/com/blexven/dependent/IndirectHello.java
+++ dependent/src/main/java/com/blexven/dependent/IndirectHello.java
@@ -2,16 +2,15 @@ package com.blexven.dependent;
 
 import com.blexven.injector.PleaseWork;
 
-@PleaseWork // this is being picked up by "mvn clean package"
 public class IndirectHello {
 
     @PleaseWork // this is being picked up by "mvn clean package"
+    private Greeting greeting;
+
     public static void main(String[] args) {
 
-        @PleaseWork // THIS IS NOT BEING PICKED UP
-                Greeting greeting = new Hello();
-
-        greeting.show();
+        IndirectHello indirectHello = new IndirectHello();
+        indirectHello.greeting.show();
 
     }
 }
