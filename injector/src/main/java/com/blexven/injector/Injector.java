2e566d51489f41a8ff94bf4c88df40a24feec47c
Cureno <14926132+Cureno@users.noreply.github.com>
2019-07-12 00:09:46 +0200

    Add file generation with dummy injection

--- injector/src/main/java/com/blexven/injector/Injector.java
+++ injector/src/main/java/com/blexven/injector/Injector.java
@@ -8,6 +8,10 @@ import javax.lang.model.element.TypeElement;
 import javax.lang.model.element.VariableElement;
 import javax.lang.model.util.Elements;
 import javax.tools.Diagnostic;
+import javax.tools.JavaFileObject;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Collections;
 import java.util.Set;
 
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
@@ -33,12 +37,56 @@ public class Injector extends AbstractProcessor {
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
 
-        for (TypeElement annotation : annotations) {
-            roundEnv.getElementsAnnotatedWith(annotation).forEach(element -> {
-                processingEnv.getMessager().printMessage(Diagnostic.Kind.WARNING, "Inject here, please: " + element);
-            });
+        for (Element element : roundEnv.getElementsAnnotatedWith(PleaseWork.class)) {
+
+            Element enclosingElement = element.getEnclosingElement();
+            VariableElement annotatedVariable = (VariableElement) element;
+
+            String generatedClassName = enclosingElement.getSimpleName() + "Injected";
+            PackageElement packageName = elementUtils.getPackageOf(element);
+
+            try {
+                JavaFileObject sourceFile = filer.createSourceFile(generatedClassName, element);
+
+                try (OutputStream out = sourceFile.openOutputStream()) {
+
+                    String type = simpleTypeOf(annotatedVariable);
+
+                    String dependencyToBeInjected = "Hello";
+                    String injectedConstructorCall = "new " + dependencyToBeInjected + "()";
+
+                    String contents =
+
+                            "package " + packageName + ";\n\n" +
+
+                            "public class " + generatedClassName + " {\n\t" +
+
+                            "private String asType = \"" + annotatedVariable.asType() + "\";\n\t" +
+
+                            "private " + type + " " + annotatedVariable + " = " + injectedConstructorCall + ";\n" +
+
+                            "}";
+
+                    out.write(contents.getBytes());
+
+                    printGeneratedContents(contents);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
         return true; // signal that we're done with processing
+    }
+
+    private void printGeneratedContents(String contents) {
+        messager.printMessage(Diagnostic.Kind.WARNING, "Contents: \n\n" + contents + "\n");
+    }
+
+
+    private String simpleTypeOf(VariableElement annotatedVariable) {
+        int lastIndex = annotatedVariable.asType().toString()
+                                         .lastIndexOf(".");
+        return annotatedVariable.asType().toString().substring(lastIndex + 1);
     }
 }
