8255444a4df60fd274caa9bf84091be6cedfe683
Cureno <14926132+Cureno@users.noreply.github.com>
2019-07-11 23:26:11 +0200

    Give better structure to the Injector class

--- injector/src/main/java/com/blexven/injector/Injector.java
+++ injector/src/main/java/com/blexven/injector/Injector.java
@@ -1,17 +1,35 @@
 package com.blexven.injector;
 
-import javax.annotation.processing.AbstractProcessor;
-import javax.annotation.processing.RoundEnvironment;
-import javax.annotation.processing.SupportedAnnotationTypes;
-import javax.annotation.processing.SupportedSourceVersion;
+import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.PackageElement;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.VariableElement;
+import javax.lang.model.util.Elements;
 import javax.tools.Diagnostic;
 import java.util.Set;
 
-@SupportedAnnotationTypes("com.blexven.injector.PleaseWork")
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
 public class Injector extends AbstractProcessor {
+
+    private Elements elementUtils;
+    private Filer filer;
+    private Messager messager;
+
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        return Collections.singleton(PleaseWork.class.getCanonicalName());
+    }
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+        elementUtils = processingEnv.getElementUtils();
+        filer = processingEnv.getFiler();
+        messager = processingEnv.getMessager();
+    }
+
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
 
